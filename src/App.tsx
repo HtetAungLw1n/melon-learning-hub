7eb47b2d8fd2883f15d768b735567a287c7d1044
Htet Aung Lwin <dev.htetaunglwin@gmail.com>
2025-05-27 14:09:01 +0630

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Main from "./layouts/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
